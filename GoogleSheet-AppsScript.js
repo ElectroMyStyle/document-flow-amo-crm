d3417ea9cfd7130ee118aceb6dd18589e533238e
ElectroMyStyle <electromystyle@mail.ru>
2025-01-11 19:10:07 +0300

    Обновлена логика обработки данных в файле GoogleSheet-AppsScript.js, добавлена дополнительная валидация данных.

--- GoogleSheet-AppsScript.js
+++ GoogleSheet-AppsScript.js
@@ -1,16 +1,54 @@
 function doPost(e) {
-    if (typeof e === 'undefined')
-        return ContentService.createTextOutput("Post Empty");
+    if (!e || !('parameter' in e) || (e.parameter === null) || (typeof e.parameter === 'undefined'))
+        return ContentService.createTextOutput("The Post request parameters are not set");
 
-    const dt = new Date(),
-        purpose_of_payment = e.parameter.purpose_of_payment,
-        payment_number = e.parameter.payment_number,
-        payment_date = e.parameter.payment_date,
-        payment_amount = e.parameter.payment_amount,
-        company_name = e.parameter.company_name;
+    const purpose_of_payment = e.parameter.purpose_of_payment,
+        document_number = e.parameter.document_number,
+        document_date_act = e.parameter.document_date_act,
+        document_payment_amount = e.parameter.document_payment_amount,
+        company_name = e.parameter.company_name,
+        document_type_id = e.parameter.document_type_id;
 
-    const sheet = SpreadsheetApp.getActive().getSheetByName('Данные');
-    sheet.appendRow([purpose_of_payment, payment_number, payment_date, payment_amount, company_name]);
+    if ((typeof purpose_of_payment !== 'string')) {
+        return ContentService.createTextOutput("The 'Payment destination' parameter is not set");
+    }
+    if ((typeof document_number !== 'string')) {
+        return ContentService.createTextOutput("The 'Document number' parameter is not set");
+    }
+    if ((typeof document_date_act !== 'string')) {
+        return ContentService.createTextOutput("The 'Date of the act' parameter is not set");
+    }
+    if ((typeof document_payment_amount !== 'string')) {
+        return ContentService.createTextOutput("The 'Payment amount' parameter is not set");
+    }
+    if ((typeof company_name !== 'string')) {
+        return ContentService.createTextOutput("The 'Company name' parameter is not set");
+    }
+    if ((typeof document_type_id !== 'string')) {
+        return ContentService.createTextOutput("The 'Document type identifier' parameter is not set");
+    }
+
+    const activeSpreadsheets = SpreadsheetApp.getActive();
+
+    if (activeSpreadsheets == null) {
+        return ContentService.createTextOutput("Active Spreadsheets not found");
+    }
+
+    let sheet = null;
+    switch (parseInt(document_type_id)) {
+        case 1: // УПД
+            sheet = activeSpreadsheets.getSheetByName('УПД 2025');
+            break;
+        case 2: // Счёт-фактуры
+            sheet = activeSpreadsheets.getSheetByName('Счет фактуры 2025');
+            break;
+    }
+
+    if (sheet == null) {
+        return ContentService.createTextOutput("Sheets not found by Document Type Id = " + document_type_id);
+    }
+
+    sheet.appendRow([purpose_of_payment, document_number, document_date_act, document_payment_amount, company_name]);
 
     return ContentService.createTextOutput("Done");
 }
